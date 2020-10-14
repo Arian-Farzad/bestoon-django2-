c15c0e199a033233d218b90d0298d7991b30cce0
Arian Farzad <arianfarzad98@gmail.com>
2020-10-14 12:33:13 +0330

    Expense model is there

--- web/models.py
+++ web/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Expense(models.Model):
+    text = models.CharField(max_length=255, default='SOME STRING')
+    date = models.DateTimeField('date_published')
+    amount = models.BigIntegerField('amount')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
